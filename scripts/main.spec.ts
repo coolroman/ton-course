222879ef2bd32b852a1fe35b3ea1c9d1326fe713
Roman Kulinsky <kulinsky.roman@gmail.com>
2024-03-25 10:37:39 +0000

    jest, first test

--- scripts/main.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from "vitest";
-
-describe("main.fc contract tests", () => {
-  test("our first test", async () => {
-    expect(3).toBe(3);
-  });
-});
---
+++ tests/main.spec.ts
@@ -0,0 +1,31 @@
+import { describe, test } from "@jest/globals";
+import { Cell, toNano } from "@ton/core";
+import { Blockchain } from "@ton/sandbox";
+import "@ton/test-utils";
+import { hex } from "../build/main.compiled.json";
+import { MainContract } from "../wrappers/MainContract";
+
+describe("main.fc contract tests", () => {
+  test("our first test", async () => {
+    const blockchain = await Blockchain.create();
+    const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+    const myContract = blockchain.openContract(
+      MainContract.createFromConfig({}, codeCell)
+    );
+    const senderWallet = await blockchain.treasury("sender");
+    const sentMessageResult = await myContract.sendInternalMessage(
+      senderWallet.getSender(),
+      toNano("0.05")
+    );
+
+    expect(sentMessageResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: true,
+    });
+
+    const data = await myContract.getData();
+
+    expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+  });
+});
