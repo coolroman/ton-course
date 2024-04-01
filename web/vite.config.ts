91ff884d638396e47183bcc4fb653da8a27e9e48
Roman Kulinsky <kulinsky.roman@gmail.com>
2024-04-01 15:07:33 +0000

    vitest

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   test: {
     reporters: ["junit"],
     root: ".",
