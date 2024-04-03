6e1d7d307d163fe15bd6f412d65b17e01930c00c
Roman Kulinsky <kulinsky.roman@gmail.com>
2024-04-03 05:20:34 +0000

    gh pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,8 +6,8 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  base: "/ton-course",
   test: {
     reporters: ["junit"],
-    root: ".",
   },
 });
