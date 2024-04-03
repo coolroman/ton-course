4a07b2974d5c33daecf0bdd9693fe41a7c392095
Roman Kulinsky <kulinsky.roman@gmail.com>
2024-04-03 08:19:48 +0000

    app layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -21,47 +21,47 @@ function App() {
   const { connected } = useTonConnect();
 
   return (
-    <div>
+    <div className="p-4">
       <div className="mb-2 flex justify-center">
         <TonConnectButton />
       </div>
       <div>
-        <div className="Card">
-          <table className="text-left">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Owner:</td>
-                <td>{owner_address?.toString()}</td>
-              </tr>
-              <tr>
-                <td>Sender:</td>
-                <td>{recent_sender_address}</td>
-              </tr>
-              <tr>
-                <td>Contract:</td>
-                <td>{contract_address}</td>
-              </tr>
-              <tr>
-                <td>Balance:</td>
-                <td className="Hint">
-                  {contract_balance == null
-                    ? "null"
-                    : fromNano(contract_balance)}
-                </td>
-              </tr>
-              <tr>
-                <td>Counter:</td>
-                <td>{counter_value ?? "Loading..."}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <table className="w-[90%] table-fixed text-left">
+          <thead>
+            <tr>
+              <th className="w-[100px]">Name</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Owner:</td>
+              <td>{owner_address?.toString()}</td>
+            </tr>
+            <tr>
+              <td>Sender:</td>
+              <td>{recent_sender_address}</td>
+            </tr>
+            <tr>
+              <td>Contract:</td>
+              <td>
+                <div className="truncate">
+                  <span>{contract_address}</span>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Balance:</td>
+              <td className="Hint">
+                {contract_balance == null ? "null" : fromNano(contract_balance)}
+              </td>
+            </tr>
+            <tr>
+              <td>Counter:</td>
+              <td>{counter_value ?? "Loading..."}</td>
+            </tr>
+          </tbody>
+        </table>
 
         {connected && (
           <>
